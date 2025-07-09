c726b13aacdeb2293ca9ad21be3655bf9c3b185e
Hossein Zare <zarehossein.hz1997@gmail.com>
2025-07-10 00:59:05 +0330

    Add `alt` to the banner image.

--- views/pages/_partials/index/banner.blade.php
+++ views/pages/_partials/index/banner.blade.php
@@ -1,3 +1,3 @@
 <section class="container">
-    <img src="{{ template_asset('/assets/images/banners/1.jpg') }}" class="rounded">
+    <img src="{{ template_asset('/assets/images/banners/1.jpg') }}" class="rounded" alt="{{ get_page_title() }}">
 </section>
