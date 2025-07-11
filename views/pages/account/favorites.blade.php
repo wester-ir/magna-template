0d0461ec08a45658b4cda364e9dd2602e82bdacb
Hossein Zare <zarehossein.hz1997@gmail.com>
2025-07-12 03:02:31 +0330

    Add `Nothing to show` message.

--- views/pages/account/favorites.blade.php
+++ views/pages/account/favorites.blade.php
@@ -8,23 +8,27 @@
             <span class="card-title">لایک‌ها</span>
         </div>
         <div class="card-content">
-            <div class="grid grid-cols-2 md:grid-cols-3 gap-5">
-                @foreach ($products as $product)
-                    <div>
-                        <a href="{{ $product->url }}">
-                            <img src="{{ $product->image->url['thumbnail'] ?? template_asset('/assets/images/no-image.jpg') }}" class="aspect-square object-cover rounded w-full">
-                        </a>
+            @if ($products->isNotEmpty())
+                <div class="grid grid-cols-2 md:grid-cols-3 gap-5">
+                    @foreach ($products as $product)
+                        <div>
+                            <a href="{{ $product->url }}">
+                                <img src="{{ $product->image->url['thumbnail'] ?? template_asset('/assets/images/no-image.jpg') }}" class="aspect-square object-cover rounded w-full">
+                            </a>
 
-                        <div class="mt-2">
-                            <div class="flex items-center justify-between">
-                                <h2 class="ml-2 text-sm truncate">{{ $product->title }}</h2>
+                            <div class="mt-2">
+                                <div class="flex items-center justify-between">
+                                    <h2 class="ml-2 text-sm truncate">{{ $product->title }}</h2>
 
-                                <x-tpl::like-product :$product />
+                                    <x-tpl::like-product :$product />
+                                </div>
                             </div>
                         </div>
-                    </div>
-                @endforeach
-            </div>
+                    @endforeach
+                </div>
+            @else
+                <div class="text-center font-light">چیزی برای نمایش وجود ندارد.</div>
+            @endif
         </div>
     </div>
 
